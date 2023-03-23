08f89ebfc650ab3ccd35f58fde99340575723aaf
Parth07kushwaha <kushwahaparth95@gmail.com>
2023-03-23 13:03:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Pagination from "./Pagination";
+import Search from "./Search";
+import Story from "./Story";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Search />
+      <Pagination />
+      <Story />
+    </>
+  );
+};
+
+export default App;
